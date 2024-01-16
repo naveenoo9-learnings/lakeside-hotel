e948aca8acf81fe2de2d4c585c4596eb23c69b96
naveenoo9 <50537003+naveenoo9@users.noreply.github.com>
2024-01-16 11:08:51 +0530

    Added Room Variables

--- src/main/java/com/tutorial/lakeside/lakesidehotel/model/Room.java
+++ src/main/java/com/tutorial/lakeside/lakesidehotel/model/Room.java
@@ -1,5 +1,13 @@
 package com.tutorial.lakeside.lakesidehotel.model;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 class Room  {
+    private Long id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked;
+    private List<BookedRoom> bookings;
   
 }
